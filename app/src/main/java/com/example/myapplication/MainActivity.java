e94355d48fdf23557d78f1b3f40968e7363ba344
erminmiro <102598749+erminmiro@users.noreply.github.com>
2024-03-11 01:48:54 +0100

    Created an event listener for the button, on click the text in the input will be copied to the response

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,10 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button checkHowLong;
+    private EditText inputField;
+    private TextView responseField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +27,16 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        checkHowLong = findViewById(R.id.button);
+
+        inputField = findViewById(R.id.editTextNumber);
+
+        responseField = findViewById(R.id.responseField);
+
+        checkHowLong.setOnClickListener(v -> {
+                responseField.setText(inputField.getText());
+        });
+
     }
 }
