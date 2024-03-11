787716f580e9a47af155fa19f86fa04e3b7fe99e
erminmiro <102598749+erminmiro@users.noreply.github.com>
2024-03-11 02:57:25 +0100

    Written a class that splits the matrNr into digits, then checks if the digit is a prime number

--- app/src/main/java/com/example/myapplication/PrimeNumberCheck.java
+++ app/src/main/java/com/example/myapplication/PrimeNumberCheck.java
@@ -4,13 +4,32 @@ import android.util.Log;
 
 public class PrimeNumberCheck {
     private String matriculationNumber;
+    private String allPrimeNumbers = "";
     public PrimeNumberCheck(String matriculationNumber) {
         this.matriculationNumber = matriculationNumber;
     }
 
     public void checkPrimeNumbers(){
         String[] singleDigits = matriculationNumber.split("(?!^)");
-        Log.d("PrimeNumbers", "checkPrimeNumbers: "+ singleDigits[1]+singleDigits[2]);
 
+        for (int i = 0; i < singleDigits.length; i++){
+            int digit = Integer.parseInt(singleDigits[i]);
+            if (isPrime(digit)){
+            allPrimeNumbers += digit;
+            }
+        }
+        Log.d("PrimeNumbers", "checkPrimeNumbers: "+ allPrimeNumbers);
+    }
+
+    //Algorithm found on https://www.geeksforgeeks.org/prime-numbers/
+    public boolean isPrime(int digit){
+        if (digit <= 1)
+            return false;
+
+        for (int i = 2; i <= Math.sqrt(digit); i++)
+            if (digit % i == 0)
+                return false;
+
+        return true;
     }
 }
